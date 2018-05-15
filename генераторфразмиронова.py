267a1fa10a72e356d3743dfde3b0c5d73339b3b6
Georgy Polonskiy <georgypolonskiy@Georgys-MacBook-Pro.local>
2018-05-15 21:22:45 +0100

    added the new word generator script, needs a few corrections

---
+++ генераторфразмиронова.py
@@ -0,0 +1,149 @@
+from random import randint
+
+ver = str(1.0)
+#def lists of words
+happy = ["а", "у", "нас", "всё", "хорошо", "поздравляю", "всех", "с", "Новым", "Годом", "сегодня", "всем", "всего", "наилучшего", "в", "Москве", "много", "плюсов", "а", "минусам", "надо", "привыкнуть"]
+philosophy = ["свобода", "совокупность", "возможностей", "определяемая", "для", "всех", "природой", "и", "определённая", "для", "себя", "живым", "существом", "дыхание", "учителей", "дыхание", "победителей", "дыхание", "их", "учеников", "дыхание", "будущих", "победителей", "причём", "больших", "чем", "сами", "учителя", "кто", "то", "ещё", "смеет", "судить", "о", "жизни", "России", "для", "чего", "он", "это", "делает", "чтобы", "не", "находились", "те", "чернила", "у", "России", "которые", "опишут", "как", "сделать", "Россию", "лучше", "и", "в", "чем", "она", "уже", "хороша"]
+patriotic = ["была", "бы", "Россия", "такой", "уж", "нищей", "не", "было", "бы", "у", "неё", "чернил", "для", "тех", "писателей", "которые", "пишут", "о", "ней", "как", "о", "нищей", "если", "у", "России", "ни", "гроша", "в", "кармане", "её", "бюджета", "то", "тогда", "на", "чьи", "гроши", "и", "на", "чьи", "чернила", "пишутся", "плохие", "отзывы", "или", "же", "кто", "то", "продаёт", "свои", "чернила", "России", "только", "потому", "что", "им", "этими", "же", "чернилами", "о", "своей", "стране", "той", "другой", "стране", "отличной", "от", "России", "в", "которой", "они", "так", "гордо", "поселились", "писать", "то", "нечего", "разве", "что", "только", "о", "бесконечных", "преступлениях", "своей", "страны"]
+science = ["между", "прочим", "один", "из", "корней", "уравнения", "посчитан", "правильно", "несмотря", "на", "убогое", "решение", "но", "наша", "кровь", "тоже", "содержит", "железо", "в", "таком", "случае", "получается", "мы", "ржавые", "изнутри", "но", "живём", "благодаря", "этому", "мудрость", "приходит", "без", "ума", "но", "остаётся", "с", "умом"]
+
+choices = ["Жизнерадостные", "Философия", "Патриотизм", "Наука"]
+
+print()
+print("Генератор фраз Миронова. Версия " + ver + ".") 
+print()
+
+def prog():
+
+    print("На какую тематику вы бы хотели цитату? Доступные варианты: жизнерадостные, философия, патриотизм, наука.")
+    print()
+
+    quote = []
+
+    user_answer = input().lower()
+
+    if user_answer == "жизнерадостные":
+        print()
+        print("""Григорий Миронов не только великий философ, но еще и потрясающий поэт!
+Популярность его цитат касательно счастья в повседневной жизни не стихает ни на минуту!
+Его цитаты печатают такие популярные издания как New York Times, Bibirevo Today и множество других!""")
+        print()
+        length_choice = input("Пожалуйста, выбирите подходящую длинну цитаты: короткая, средняя, длинная: ").lower()
+        if length_choice == "короткая":
+            for x in range(6):
+                quote.append(happy[randint(0, (len(happy) - 1))])
+            print()
+            final_quote = (" ".join(quote))
+            print(final_quote.capitalize() + ".")
+            print()
+        elif length_choice == "средняя":
+            for x in range(12):
+                quote.append(happy[randint(0, (len(happy) - 1))])
+            print()
+            final_quote = (" ".join(quote))
+            print(final_quote.capitalize() + ".")
+            print()
+        elif length_choice == "длинная":
+            for x in range(18):
+                quote.append(happy[randint(0, (len(happy) - 1))])
+            print()
+            final_quote = (" ".join(quote))
+            print(final_quote.capitalize() + ".")
+            print()
+
+
+
+    elif user_answer == "философия":
+        print()
+        print("""Философские цитаты Григория Миронова уже не один год будоражат умы передовых представителей философской мысли.
+Его потрясающая умение находит позиции касательно любого вопроса (даже в тех, в которых он не разбирается), 
+всегда находит отклик в душах его почитателей. Вы определенно сделали правильный выбор!""")
+        print()
+        length_choice = input("Пожалуйста, выбирите подходящую длинну цитаты: короткая, средняя, длинная: ").lower()
+        if length_choice == "короткая":
+            for x in range(6):
+                quote.append(philosophy[randint(0, (len(philosophy) - 1))])
+            print()
+            final_quote = (" ".join(quote))
+            print(final_quote.capitalize() + ".")
+            print()
+        elif length_choice == "средняя":
+            for x in range(12):
+                quote.append(philosophy[randint(0, (len(philosophy) - 1))])
+            print()
+            final_quote = (" ".join(quote))
+            print(final_quote.capitalize() + ".")
+            print()
+        elif length_choice == "длинная":
+            for x in range(18):
+                quote.append(philosophy[randint(0, (len(philosophy) - 1))])
+            print()
+            final_quote = (" ".join(quote))
+            print(final_quote.capitalize() + ".")
+            print()
+
+    elif user_answer == "патриотизм":
+        print()
+        print("""Патриотические изыскания Григория Миронова уже не один год впечатляют своей глубиной людей искренне любящих свою страну.
+Его поэзия и проза насквозь пропитаны любовью к своей отчизне. 
+Этот выбор определенно является хорошей точкой для старта своего дня для любителей Григория любого уровня.""")
+        print()
+        length_choice = input("Пожалуйста, выбирите подходящую длинну цитаты: короткая, средняя, длинная: ").lower()
+        if length_choice == "короткая":
+            for x in range(6):
+                quote.append(patriotic[randint(0, (len(patriotic) - 1))])
+            print()
+            final_quote = (" ".join(quote))
+            print(final_quote.capitalize() + ".")
+            print()
+        elif length_choice == "средняя":
+            for x in range(12):
+                quote.append(patriotic[randint(0, (len(patriotic) - 1))])
+            print()
+            final_quote = (" ".join(quote))
+            print(final_quote.capitalize() + ".")
+            print()
+        elif length_choice == "длинная":
+            for x in range(18):
+                quote.append(patriotic[randint(0, (len(patriotic) - 1))])
+            print()
+            final_quote = (" ".join(quote))
+            print(final_quote.capitalize() + ".")
+            print()
+
+    elif user_answer == "наука":
+        print()
+        print("""Научные познания Григория уже не один год потрясают умы ученых из разных областей.
+Его умопомрачительные знания в любой научной области удивят любого, и даже Вас!""")
+        print()
+        length_choice = input("Пожалуйста, выбирите подходящую длинну цитаты: короткая, средняя, длинная: ").lower()
+        if length_choice == "короткая":
+            for x in range(6):
+                quote.append(science[randint(0, (len(science) - 1))])
+            print()
+            final_quote = (" ".join(quote))
+            print(final_quote.capitalize() + ".")
+            print()
+        elif length_choice == "средняя":
+            for x in range(12):
+                quote.append(science[randint(0, (len(science) - 1))])
+            print()
+            final_quote = (" ".join(quote))
+            print(final_quote.capitalize() + ".")
+            print()
+        elif length_choice == "длинная":
+            for x in range(18):
+                quote.append(science[randint(0, (len(science) - 1))])
+            print()
+            final_quote = (" ".join(quote))
+            print(final_quote.capitalize() + ".")
+            print()
+
+    else:
+        print()
+        print("Извините, такого варианта не существует.")
+        print()
+        prog()
+        
+
+prog()
